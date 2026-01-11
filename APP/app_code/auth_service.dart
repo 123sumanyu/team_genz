b2713daebbe72aa4c8bda6fe45ea3a21f9d47d3b
SUMANYU SINGH <77450102+123sumanyu@users.noreply.github.com>
2026-01-11 23:47:06 +0530

    changes

---
+++ APP/app_code/auth_service.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Register
+  Future<User?> register(String email, String password) async {
+    UserCredential cred = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return cred.user;
+  }
+
+  // Login
+  Future<User?> login(String email, String password) async {
+    UserCredential cred = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return cred.user;
+  }
+
+  // Logout
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  // Current User
+  User? get currentUser => _auth.currentUser;
+}
